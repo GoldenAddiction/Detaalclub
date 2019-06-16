9acce923c062437f191bdd24e46da8de521e6a70
lambr <florian.lambrecht@student.howest.be>
2019-06-16 18:05:55 +0200

    SlideShow script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,21 @@
 'use strict';
-
-
-
+let imageIndex = 0;
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     slideShow();
-    window.setInterval(slideShow, 8000);
+    //1000 = 1 seconde
+    window.setInterval(slideShow, 5000);
 }
 
 function slideShow() {
-    document.querySelector('#slideShown').innerHTML = `<img src="assets/images/img_lights_wide.jpg">`;
-}
-
-/*"use strict";
-
-let pawnIndex = 0;
-
-document.addEventListener("DOMContentLoaded", init);
-
-function init() {
-    randomTip();
-    window.setInterval(randomTip, 8000);
-    fetchMatchMakingStatus();
-    document.querySelector("#readyButton").addEventListener("click",playerReady);
-}
-
-function randomTip() {
     let randomNumber = 0;
-    while (pawnIndex === randomNumber) {
-        randomNumber = Math.floor(Math.random() * pawns.length);
+    while (imageIndex === randomNumber) {
+        randomNumber = Math.floor(Math.random() * imagesSlideShow.length);
     }
-    pawnIndex = randomNumber;
-    let nedName = pawns[pawnIndex].nedName;
-    let img = `assets/image/${pawns[pawnIndex].image}`;
-    let tip = pawns[pawnIndex].tip;
-    document.querySelector('#randomTip').innerHTML = `<img alt="${nedName}" title="${nedName}" src=${img} ><p>${tip}</p>`;
-}*/
+    imageIndex = randomNumber;
+    let alt = imagesSlideShow[imageIndex].name;
+    let img = `assets/slideShowImages/${imagesSlideShow[imageIndex].image}`;
+    document.querySelector('#slideShow').innerHTML = `<img alt="${alt}" title="${alt}" src=${img} >`;
+
+}
