580cd37075c44dedc77f6ab1b9625d547a6aa110
maarten vansever <maarten.vansever@student.howest.be>
2019-06-24 14:14:37 +0200

    Grid voor phone van team

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,8 @@
-'use strict';
-let imageIndex = 0;
+"use Strict";
 document.addEventListener('DOMContentLoaded', init);
-
 function init() {
-    document.querySelector('#dots').addEventListener('click', clickedOnDot);
     document.querySelector('#menu').addEventListener('click', showDropDownMenu);
-   // let width = window.matchMedia("(max-width: 768px)");
-   // checkScreenSize(width);
-   // width.addListener(checkScreenSize);
-    //1000 = 1 seconde
-    window.setInterval(slideShow, 8000);
-    dots();
+
 }
 
 function showDropDownMenu(e) {
@@ -18,51 +10,7 @@ function showDropDownMenu(e) {
     if (document.querySelector('#dropDown').classList.contains("hidden") ){
         document.querySelector('#dropDown').classList.replace("hidden", "none");
     } else {
-        console.log("test");
         document.querySelector('#dropDown').classList.replace("none", "hidden");
     }
 }
 
-function dots() {
-    for (let img of imagesSlideShow) {
-        document.querySelector('#dots').innerHTML += `<span class="dot" title="${img.name}" id="dot-${img.id}"></span>`;
-    }
-}
-
-function slideShow() {
-    removeDotColor();
-    if (imageIndex === imagesSlideShow.length-1){
-        imageIndex = 0;
-    } else {
-        imageIndex +=1;
-    }
-    let alt = imagesSlideShow[imageIndex].name;
-    let img = `assets/slideShowImages/${imagesSlideShow[imageIndex].image}`;
-    document.querySelector('#slideShow').innerHTML = `<img alt="${alt}" title="${alt}" src=${img} class="fade" >`;
-    document.querySelector(`#dot-${imageIndex}`).classList.replace('dot', 'selectedDot');
-}
-
-function clickedOnDot(e) {
-    if (e.target.className === "dot") {
-        let clickedImg = e.target.id;
-        let id = Number(clickedImg.substring(4, clickedImg.length));
-        let img = `assets/slideShowImages/${imagesSlideShow[id].image}`;
-        document.querySelector('#slideShow').innerHTML = `<img alt="${imagesSlideShow[id].name}" title="${imagesSlideShow[id].name}" src=${img} class="fade" >`;
-        removeDotColor();
-        document.querySelector(`#dot-${id}`).classList.replace('dot', 'selectedDot');
-        imageIndex = id;
-    }
-}
-
-function removeDotColor() {
-    for (let img of imagesSlideShow ) {
-        document.querySelector(`#dot-${img.id}`).classList.replace('selectedDot', 'dot');
-    }
-}
-function checkScreenSize(width){
-    if (width.matches) { // If media query matches
-        console.log("red")
-    } else {
-        console.log("blue")
-    }
-}
